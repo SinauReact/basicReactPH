4143f278bfb1e26d1828baf26697949439236c31
argianardi <argianardi1435@gmail.com>
2022-08-27 07:48:48 +0700

    fetching  post method

--- src/pages/BlogPost/BlogPost.jsx
+++ src/pages/BlogPost/BlogPost.jsx
@@ -6,13 +6,16 @@ import Post from "../../component/Post/Post";
 
 const BlogPost = () => {
   const [post, setPost] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [id, setId] = useState(0);
 
   useEffect(() => {
-    apiGetPost();
+    apiGet();
   }, []);
 
-  // fetch Request API with get method
-  //   const apiGetPost = () => {
+  // fetch Request API get method
+  //   const apiGet = () => {
   //     fetch("https://jsonplaceholder.typicode.com/posts")
   //       .then((response) => response.json())
   //       .then((json) => {
@@ -24,10 +27,10 @@ const BlogPost = () => {
   //       });
   //   };
 
-  //   axios Request API with get method
-  const apiGetPost = () => {
+  //   axios Request API  get method
+  const apiGet = () => {
     axios
-      .get("http://localhost:3004/posts")
+      .get("http://localhost:3004/posts?_sort=id&_order=desc")
       .then((response) => {
         setPost(response.data);
       })
@@ -36,16 +39,66 @@ const BlogPost = () => {
       });
   };
 
-  //   axios Delete API
+  //   axios Request API Delete method
   const handleRemove = (id) => {
     axios.delete(`http://localhost:3004/posts/${id}`).then((res) => {
-      apiGetPost();
+      apiGet();
     });
+  };
+
+  // axios Request API with Post method
+
+  const bodyReq = {
+    id,
+    title,
+    body,
+  };
+
+  const apiPost = () => {
+    let timestamp = new Date().getTime();
+    setId(timestamp);
+    axios
+      .post("http://localhost:3004/posts", bodyReq)
+      .then((response) => {
+        apiGet();
+      })
+      .catch((error) => {
+        alert("the post is failed");
+      });
+  };
+
+  //   handleSubmit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    apiPost();
+    console.log(e);
   };
 
   return (
     <>
-      <p>Blog post</p>
+      <p className="section-title">Blog post</p>
+
+      {/* Form */}
+      <form className="form-add-post" onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          placeholder="add title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="body-content">Blog Content</label>
+        <textarea
+          id="body-content"
+          cols="30"
+          rows="10"
+          placeholder="add blog content"
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <button className="btn-submit">Simpan</button>
+      </form>
+
+      {/* card */}
       {post.map((item) => {
         return (
           <Post
